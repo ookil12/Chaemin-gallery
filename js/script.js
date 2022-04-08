cb66a982ad0e23ec0670d1afdc9440f5686451d8
ookil12 <ookil12@gmail.com>
2022-04-09 00:10:05 +0900

    optimization

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 var isshowed = 0;
 function showWarning() {
   $("#contents").animate({ opacity: "0.1" }, 1000);
-  $("#warning").animate({ opacity: "1" }, 1000, function () {
+  $("#warning").animate({ opacity: "0.99" }, 1000, function () {
     isshowed = 1;
   });
 }
 function hideWarning() {
   if (isshowed == 1) {
-    $("#contents").animate({ opacity: "1" }, 1000);
+    $("#contents").animate({ opacity: "0.99" }, 1000);
     $("#warning").animate({ opacity: "0" }, 1000);
     isshowed = 0;
   }
 }
 function showWarning1() {
   $("#contents").animate({ opacity: "0" }, 1000);
-  $("#warningsize").animate({ opacity: "1" }, 1000);
+  $("#warningsize").animate({ opacity: "0.99" }, 1000);
 }
 function hideWarning1() {
-  $("#contents").animate({ opacity: "1" }, 1000);
+  $("#contents").animate({ opacity: "0.99" }, 1000);
   $("#warningsize").animate({ opacity: "0" }, 1000);
 }
 $(document).ready(function () {
